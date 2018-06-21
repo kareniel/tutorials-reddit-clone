b222a249943fbd4fc65e416a33052ed07c0aa7c9
Jonathan Dupré <hello@jonathandupre.com>
2018-06-21 12:10:17 -0400

    base href & pushstate, routes & views, layout & css reset

--- src/router.js
+++ src/router.js
@@ -1,7 +1,12 @@
-var mainView = require('./views/main.view')
+var layout = require('./layout')
+var main = require('./views/main.view')
+var submit = require('./views/submit.view')
+var comments = require('./views/comments.view')
 
 module.exports = router
 
 function router (state, emitter, app) {
-  app.route('/', mainView)
+  app.route('/', layout(main))
+  app.route('/submit', layout(submit))
+  app.route('/posts/:postId/comments', layout(comments))
 }
---
+++ src/views/comments.view.js
@@ -0,0 +1,10 @@
+var html = require('choo/html')
+
+module.exports = commentsView
+
+function commentsView () {
+  return html`
+    <main>
+      
+    </main>`
+}
